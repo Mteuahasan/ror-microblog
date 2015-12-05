473100631a747becb6a17fb70f031883038aafc7
Matthieu Lachassagne <matthieu.lachassagne@hetic.net>
2015-12-05 16:31:01 +0100

    Add user update action and email format verification

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.update(params[:id], user_params)
+    if @user.save
+      render :nothing => true, :status => :created
+    else
+      render :json => { :errors => @user.errors.messages }, :status => :bad_request
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:pseudo, :first_name, :last_name, :email, :password)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :pseudo, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :password, presence: true
 end
