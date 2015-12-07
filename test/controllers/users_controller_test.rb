002cb19c2387558b550ca111d8ba229e7bd5eda1
hugocaillard <c.hugo.188@gmail.com>
2015-12-07 19:45:00 +0100

    Improve user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,12 +5,43 @@ class UsersControllerTest < ActionController::TestCase
     @controller = Api::V1::UsersController.new
   end
 
-  test "should exists a route to create user" do
+  test "should route to create user" do
     assert_routing({ method: 'post', path: '/api/v1/users' }, { controller: "api/v1/users", action: "create" })
   end
 
   test "should create an user" do
-    post :create, user: {pseudo: "Mteuahasan", first_name: "Matthieu", last_name:"Lachassagne", email: "hello@matthieulachassagne.com", password: "password1"}
+    create_user
     assert_response :success
+  end
+
+  test "should not create duplicate user" do
+    create_user
+    create_user
+    assert_response :bad_request
+  end
+
+  test "should not provide authentication token" do
+    authenticate password: "wrong_password"
+    assert_response :unauthorized
+  end
+
+  test "should provide authentication token" do
+    authenticate
+    body = JSON.parse response.body
+    assert_not_empty body["auth_token"]
+    assert_equal body["auth_token"].length, 132
+    assert_equal "Mteuahasan", body["user"]["pseudo"]
+    assert_response :success
+  end
+
+  private
+
+  def create_user
+    post :create, user: {pseudo: "Mteuahasan", first_name: "Matthieu", last_name:"Lachassagne", email: "hello@matthieulachassagne.com", password: "password1"}
+  end
+
+  def authenticate(username: "Mteuahasan", password: "password1")
+    create_user
+    post :authenticate, {pseudo: username, password: password}
   end
 end
